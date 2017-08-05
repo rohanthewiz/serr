1db74ea7d20de80c0ee05427b68d6148801cf5c1
Rohan Allison <rohanthewiz_at_gmail.com>
2017-08-05 12:48:58 -0500

    initial commit

---
+++ serr.go
@@ -0,0 +1,69 @@
+package serr
+
+// Structured Error wrapper
+// Supports wrapping of errors with a list of key, values to nicely support structured logging
+// Works nicely with logger.LogErr
+type SErr struct {
+	err error  // the usual error
+	// support structured logging of the format key1, val1, key2, val2
+	// Repeated keys are allowed and should be concatenated in log output
+	fields []string
+}
+
+func NewSErr(err error, fields []string) error {
+	se := SErr{}
+	se.err = err
+	se.fields = fields
+	return se
+}
+
+// Yes we are also an error type -- sweet!
+// Satisfy the `error` interface
+func (se SErr) Error() string {
+	return se.err.Error()
+}
+
+// Return all SErr attributes as a map of string keys and values
+func (se SErr) FieldsMap() map[string]string {
+	flds := map[string]string{}
+	key := ""
+	for i, str := range se.fields {
+		if i % 2 == 0 {  // even indices are presumed to be keys
+			key = str
+		} else {
+			if orig, ok := flds[key]; ok {  // we've seen this key before
+				flds[key] = str + " - " + orig
+			} else {
+				flds[key] = str
+			}
+		}
+	}
+	return flds
+}
+
+// Wrap an existing error. Attribute keys and values must be strings.
+// This requires an even number of fields unless a single field is given in which case it is added under the key "msg".
+// Returns an SErr (structured err)
+func Wrap(err error, fields ...string) error {
+	var flds []string
+	if se, ok := err.(SErr); ok && len(se.fields) > 0 {
+		flds = append(flds, se.fields...)  // add existing fields first
+	}
+
+	if len(fields) == 1 {
+		flds = append(flds, []string{"msg", fields[0]}...)
+	} else {
+		flds = append(flds, fields...)
+	}
+	return SErr{err, flds}  // return
+}
+
+// Return the wrapped error
+func (se SErr) GetWrappedErr() error {
+	return se.err
+}
+
+// Return the internal list of keys and values
+func (se SErr) FieldsSlice() []string {
+	return se.fields
+}
---
+++ serr_test.go
@@ -0,0 +1,86 @@
+package serr
+
+import (
+	"testing"
+	"fmt"
+	"errors"
+)
+
+func TestSErr(t *testing.T) {
+	const strErr1 = "Ok. This is a test err"
+	// todo: constantize most test literals
+
+	fmt.Println("Testing SErr")
+	ser := NewSErr(
+		errors.New(strErr1),
+		[]string{"thing1", "thing1val", "thing2", "thing2val"},
+	)
+	if ser.Error() != strErr1 {
+		t.Errorf("Expected custom error to contain '%s', got '%s'", strErr1, ser.Error())
+		t.FailNow()
+	}
+	if _, ok := ser.(SErr); !ok {
+		t.Error("ser should be a SErr")
+		t.FailNow()
+	}
+
+	// Add some fields to an existing customErr
+	err := Wrap(ser, "thing2", "thing2NewVal")
+	se, ok := err.(SErr)
+	if !ok {
+		t.Error("Wrap should return an error containing a concrete SErr type")
+	} else {
+		// Test SErr#GetWrappedErr
+		strErr := se.GetWrappedErr().Error()
+		if strErr != strErr1 {
+			t.Errorf(`Expected wrapped error string to be "%s", got "%s"`, strErr1, strErr)
+		}
+		// Test SErr#FieldsSlice
+		strFlds := se.FieldsSlice()
+		fmt.Println("[Debug] strFlds:", strFlds)  // debug
+		if len(strFlds) != 6 {
+			t.Error("Expected length of customErr.Fields() to be 6, got", len(strFlds))
+		}
+		// Test SErr#FieldsMap
+		mapFlds := se.FieldsMap()
+		if len(mapFlds) != 2 {
+			t.Error("Expected length of customErr.MapFlds() to be 2, got", len(mapFlds))
+			t.FailNow()
+		}
+
+		if val, ok := mapFlds["thing1"]; ok {
+			if val != "thing1val" {
+				t.Errorf("Expected thing1 to be 'thing1Val', got '%s'", val)
+			}
+		} else {
+			t.Error("mapFlds should contain the key: thing1")
+		}
+
+		if val, ok := mapFlds["thing2"]; ok {
+			if val != "thing2NewVal - thing2val" {
+				t.Errorf("Expected thing2 to be 'thing2NewVal - thing2val', got '%s'", val)
+			}
+		} else {
+			t.Error("mapFlds should contain the key: thing2")
+		}
+	}
+
+	// We should be able to wrap with a single field which becomes `"msg": field`
+	const thisIsMyMessage = "This is my message"
+	er := Wrap(errors.New(strErr1), thisIsMyMessage)
+	se, ok = er.(SErr)
+	if !ok {
+		t.Error("er should be a SErr"); t.FailNow()
+	}
+	sl := se.FieldsSlice()
+	if len(sl) != 2 {
+		t.Error("Structured error from an error wrapped with a single field should contain two fields")
+	}
+	if len(sl) > 0 && sl[0] != "msg" {
+		t.Error("Structured error from an error wrapped with a single field should have 'map' as the first field")
+	}
+	if len(sl) > 1 && sl[1] != thisIsMyMessage {
+		t.Errorf(`The structured error should have "%s" as the second field, got "%s"`, thisIsMyMessage, sl[1])
+		fmt.Println()
+	}
+}
